04a6687d38962bce0a4e9071dec56cbcef7a58d8
sptkl <caobaiyue@gmail.com>
2020-03-12 01:31:59 +0000

    [build]

--- capitalprojects_build/python/dataloading.py
+++ capitalprojects_build/python/dataloading.py
@@ -25,18 +25,16 @@ def ETL():
     importer.import_table(schema_name='dcp_trafficanalysiszones')
     importer.import_table(schema_name='dpr_parksproperties')
     importer.import_table(schema_name='dpr_capitalprojects')
-    importer.import_table(schema_name='dot_projects_bridges') #
-    importer.import_table(schema_name='dot_projects_streets') #
-    importer.import_table(schema_name='dot_projects_intersections') #
-    importer.import_table(schema_name='ddc_capitalprojects_infrastructure') #
-    importer.import_table(schema_name='ddc_capitalprojects_publicbuildings') #
-    importer.import_table(schema_name='edc_capitalprojects') #?
-    importer.import_table(schema_name='edc_capitalprojects_ferry') #?
-    importer.import_table(schema_name='sca_bluebook', version='2019') 
-    importer.import_table(schema_name='capital_spending') #?
-    importer.import_table(schema_name='doe_lcgms', version='2019_new')
+    importer.import_table(schema_name='dot_projects_bridges')
+    importer.import_table(schema_name='dot_projects_streets')
+    importer.import_table(schema_name='dot_projects_intersections')
+    importer.import_table(schema_name='ddc_capitalprojects_infrastructure')
+    importer.import_table(schema_name='ddc_capitalprojects_publicbuildings')
+    importer.import_table(schema_name='edc_capitalprojects')
+    importer.import_table(schema_name='edc_capitalprojects_ferry')
+    importer.import_table(schema_name='capital_spending')
     importer.import_table(schema_name='dcp_school_districts')
-    importer.import_table(schema_name='fisa_capitalcommitments') #? 
+    importer.import_table(schema_name='fisa_capitalcommitments') 
     importer.import_table(schema_name='dcp_cpdb_agencyverified')
     
 def LOAD(url, name):
