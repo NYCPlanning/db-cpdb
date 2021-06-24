87d710f36a0f8d5ce36786e6ce9f58e2f34e47ff
sptkl <caobaiyue@gmail.com>
2021-06-24 11:52:24 -0400

    rewrite archive all to include all tables and intermediary tables

--- cpdb.sh
+++ cpdb.sh
@@ -10,32 +10,26 @@ function cpdb_archive {
     shift;
     case $1 in
     --all)
-        archive public.cpdb_summarystats_magency cpdb.cpdb_summarystats_magency &
-        archive public.cpdb_summarystats_sagency cpdb.cpdb_summarystats_sagency &
-        archive public.projects_by_communitydist cpdb.projects_by_communitydist &
-        archive public.projects_by_communitydist_spending cpdb.projects_by_communitydist_spending
-        archive public.projects_by_communitydist_spending_date cpdb.projects_by_communitydist_spending_date &
-        archive public.agency_validated_geoms_summary_table cpdb.agency_validated_geoms_summary_table &
-        archive public.cpdb_dcpattributes_pts cpdb.cpdb_dcpattributes_pts &
-        archive public.cpdb_dcpattributes_poly cpdb.cpdb_dcpattributes_poly &
-        archive public.cpdb_adminbounds cpdb.cpdb_adminbounds &
-        archive public.cpdb_projects_combined cpdb.cpdb_projects_combined &
-        archive public.cpdb_commitments cpdb.cpdb_commitments &
-        archive public.cpdb_projects cpdb.cpdb_projects &
-        archive public.cpdb_budgets cpdb.cpdb_budgets &
-        archive public.cpdb_projects_spending_byyear cpdb.cpdb_projects_spending_byyear &
-        archive public.cpdb_capital_spending cpdb.cpdb_capital_spending &
-        archive public.fisa_capitalcommitments cpdb.fisa_capitalcommitments &
-        archive public.dot_projects_intersections cpdb.dot_projects_intersections &
-        archive public.dot_projects_streets cpdb.dot_projects_streets &
-        archive public.dot_projects_bridges cpdb.dot_projects_bridges &
-        archive public.dpr_capitalprojects cpdb.dpr_capitalprojects &
-        archive public.dpr_parksproperties cpdb.dpr_parksproperties &
-        archive public.edc_capitalprojects_ferry cpdb.edc_capitalprojects_ferry &
-        archive public.edc_capitalprojects cpdb.edc_capitalprojects &
-        archive public.dcp_cpdb_agencyverified cpdb.dcp_cpdb_agencyverified &
-        archive public.ddc_capitalprojects_infrastructure cpdb.ddc_capitalprojects_infrastructure &
-        archive public.ddc_capitalprojects_publicbuildings cpdb.ddc_capitalprojects_publicbuildings &
+        psql $BUILD_ENGINE\
+        --no-align \
+        --tuples-only \
+        --field-separator ' ' \
+        --pset footer=off \
+        -c "
+        SELECT table_name 
+        FROM information_schema.tables 
+        WHERE table_schema = 'public'
+        AND table_name not in (
+            'geography_columns', 
+            'geometry_columns', 
+            'raster_columns', 
+            'raster_overviews', 
+            'spatial_ref_sys'
+        );
+        " | while read -a Record ; do
+            local table_name="${Record[0]}"
+            archive public.$table_name cpdb.$table_name &
+        done
         wait
         echo "Archive Complete"
         ;;
