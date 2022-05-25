baf1733b11858b4cc4582f90fa8e70f911b81728
Max <max@DCP-APPLE-1991.local>
2022-05-25 15:08:52 -0400

    add cpdb_dcpattributes table to export for debug

--- bash/05_export.sh
+++ bash/05_export.sh
@@ -18,6 +18,7 @@ mkdir -p output && (
     CSV_export cpdb_budgets &
     CSV_export cpdb_projects_spending_byyear &
     CSV_export ddc_capitalprojects_infrastructure &
+    CSV_export cpdb_dcpattributes &
     wait 
     echo 
     echo "export complete"
