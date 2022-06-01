5342a3b6a586da1b2744b25a1ae0f21ba63d895e
Sasha Weinstein <70385853+SashaWeinstein@users.noreply.github.com>
2022-06-01 17:12:33 -0400

    clear unused line

--- sql/attributes_geomclean.sql
+++ sql/attributes_geomclean.sql
@@ -4,7 +4,6 @@ UPDATE cpdb_dcpattributes
 SET geom=ST_Buffer(geom::geography, 15)::geometry
 WHERE ST_GeometryType(geom) = 'ST_MultiLineString';
 
- 
 --Update geom to be multi
 UPDATE cpdb_dcpattributes
 SET geom=ST_Multi(geom)
