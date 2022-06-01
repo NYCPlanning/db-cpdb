284cf884d7996ebc86287d28046895db0fbe311c
Sasha Weinstein <70385853+SashaWeinstein@users.noreply.github.com>
2022-06-01 15:07:47 -0400

    put conversion of lines to polygons in for loop so it doesn't overwhelm memeory

--- sql/attributes_geomclean.sql
+++ sql/attributes_geomclean.sql
@@ -1,9 +1,20 @@
 --Update lines to be polygons
-UPDATE cpdb_dcpattributes
-SET geom=ST_Buffer(geom::geography, 15)::geometry
-WHERE ST_GeometryType(geom) = 'ST_MultiLineString';
+-- UPDATE cpdb_dcpattributes
+-- SET geom=ST_Buffer(geom::geography, 15)::geometry
+-- WHERE ST_GeometryType(geom) = 'ST_MultiLineString';
 
+
+do $$
+BEGIN
+    FOR i IN 1..(SELECT(count(maprojid)) from cpdb_dcpattributes) by 100 LOOP 
+        UPDATE cpdb_dcpattributes 
+        SET geom=ST_Buffer(geom::geography, 15)::geometry
+        WHERE ST_GeometryType(geom) = 'ST_MultiLineString' AND maprojid IN 
+        (SELECT maprojid from cpdb_dcpattributes WHERE
+         ST_GeometryType(geom) = 'ST_MultiLineString' LIMIT 100);
+    END LOOP; 
 --Update geom to be multi
 UPDATE cpdb_dcpattributes
 SET geom=ST_Multi(geom)
 WHERE ST_GeometryType(geom) in ('ST_Polygon', 'ST_Point');
+end; $$
