70a65c78a650ee2a6e26195f3d49a0e33b6edd91
Sasha Weinstein <70385853+SashaWeinstein@users.noreply.github.com>
2022-06-01 16:58:08 -0400

    try with just st snap to geometry, no for loop

--- sql/attributes_geomclean.sql
+++ sql/attributes_geomclean.sql
@@ -1,22 +1,23 @@
 --Update lines to be polygons
--- UPDATE cpdb_dcpattributes
--- SET geom=ST_Buffer(geom::geography, 15)::geometry
--- WHERE ST_GeometryType(geom) = 'ST_MultiLineString';
-
-
-do $$
-BEGIN
 UPDATE cpdb_dcpattributes SET geom = ST_SnapToGrid(geom, .001);
+UPDATE cpdb_dcpattributes
+SET geom=ST_Buffer(geom::geography, 15)::geometry
+WHERE ST_GeometryType(geom) = 'ST_MultiLineString';
 
-    FOR i IN 1..(SELECT(count(maprojid)) from cpdb_dcpattributes) by 100 LOOP 
-        UPDATE cpdb_dcpattributes 
-        SET geom=ST_Buffer(geom::geography, 15)::geometry
-        WHERE ST_GeometryType(geom) = 'ST_MultiLineString' AND maprojid IN 
-        (SELECT maprojid from cpdb_dcpattributes WHERE
-         ST_GeometryType(geom) = 'ST_MultiLineString' LIMIT 100);
-    END LOOP; 
+
+-- do $$
+-- BEGIN
+-- UPDATE cpdb_dcpattributes SET geom = ST_SnapToGrid(geom, .001);
+
+--     FOR i IN 1..(SELECT(count(maprojid)) from cpdb_dcpattributes) by 100 LOOP 
+--         UPDATE cpdb_dcpattributes 
+--         SET geom=ST_Buffer(geom::geography, 15)::geometry
+--         WHERE ST_GeometryType(geom) = 'ST_MultiLineString' AND maprojid IN 
+--         (SELECT maprojid from cpdb_dcpattributes WHERE
+--          ST_GeometryType(geom) = 'ST_MultiLineString' LIMIT 100);
+--     END LOOP; 
 --Update geom to be multi
 UPDATE cpdb_dcpattributes
 SET geom=ST_Multi(geom)
 WHERE ST_GeometryType(geom) in ('ST_Polygon', 'ST_Point');
-end; $$
+-- end; $$
