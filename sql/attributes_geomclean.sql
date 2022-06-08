c19f62b361c2176e56274b0108ad204765fc068f
Sasha Weinstein <70385853+SashaWeinstein@users.noreply.github.com>
2022-06-08 12:28:34 -0400

    only snap lines, snap to finer grid, and don't call make valid

--- sql/attributes_geomclean.sql
+++ sql/attributes_geomclean.sql
@@ -1,5 +1,5 @@
 --Update lines to be polygons
-UPDATE cpdb_dcpattributes SET geom = ST_SnapToGrid(geom, .001);
+UPDATE cpdb_dcpattributes SET geom = ST_SnapToGrid(geom, .00001) WHERE ST_GeometryType(geom) ='ST_MultiLineString';
 UPDATE cpdb_dcpattributes
 SET geom=ST_Buffer(geom::geography, 15)::geometry
 WHERE ST_GeometryType(geom) = 'ST_MultiLineString';
@@ -10,6 +10,6 @@ SET geom=ST_Multi(geom)
 WHERE ST_GeometryType(geom) in ('ST_Polygon', 'ST_Point');
 
 --Make the geom valid
-UPDATE cpdb_dcpattributes
-SET geom=ST_MakeValid(geom)
-WHERE ST_IsValid(geom) = FALSE;
+-- UPDATE cpdb_dcpattributes
+-- SET geom=ST_MakeValid(geom)
+-- WHERE ST_IsValid(geom) = FALSE;
