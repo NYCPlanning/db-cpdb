b8cc375a3e3bf32fd242ecddfd640022d14f227c
Casey Smith <caseysmith@DCP-APPLE-2152.local>
2022-08-01 15:30:55 -0400

    updated select into language for testing

--- sql/opendata.sql
+++ sql/opendata.sql
@@ -39,100 +39,99 @@ and c.maprojid = b.maprojid;
 
 --Create tabular cpdb projects table
 DROP TABLE IF EXISTS cpdb_opendata_projects;
-CREATE TABLE cpdb_opendata_projects AS (
-    WITH summary AS (
-    SELECT p.maprojid,
-    p.magency, 
-    p.projectid, 
-    p.description, 
-    p.ccnonexempt,
-    p.ccexempt, 
-    p.citycost AS totalcityplannedcommit,
-    p.nccstate, 
-    p.nccfederal, 
-    p.nccother, 
-    p.noncitycost AS totalnoncityplannedcommit, 
-    p.totalcost AS totalplannedcommit, 
-    --pc.totalspend,
-    --pc.maxdate, 
-    --pc.mindate,
-    p.magencyacro,
-    p.magencyname,
-    --pc.typecategory,
-    p.ccpversion
-    FROM cpdb_projects as p,
-    --cpdb_projects_combined as pc
-    WHERE p.maprojid = pc.maprojid
-    )
-SELECT s.*
-FROM summary s);
+WITH proj AS (
+SELECT 
+p.maprojid,
+p.magency, 
+p.projectid, 
+p.description, 
+p.ccnonexempt,
+p.ccexempt, 
+p.citycost AS totalcityplannedcommit,
+p.nccstate, 
+p.nccfederal, 
+p.nccother, 
+p.noncitycost AS totalnoncityplannedcommit, 
+p.totalcost AS totalplannedcommit, 
+pc.totalspend,
+pc.maxdate, 
+pc.mindate,
+p.magencyacro,
+p.magencyname,
+pc.typecategory,
+p.ccpversion
+FROM cpdb_projects as p,
+    cpdb_projects_combined as pc
+WHERE p.maprojid = pc.maprojid)
+SELECT p.*
+INTO cpdb_opendata_projects
+FROM proj as p;
 
 DROP TABLE IF EXISTS cpdb_opendata_projects_pts;
-CREATE TABLE cpdb_opendata_projects_pts AS (
-    WITH summary AS (
-    SELECT p.maprojid,
-    p.magency, 
-    p.projectid, 
-    p.description, 
-    p.ccnonexempt,
-    p.ccexempt, 
-    p.citycost AS totalcityplannedcommit,
-    p.nccstate, 
-    p.nccfederal, 
-    p.nccother, 
-    p.noncitycost AS totalnoncityplannedcommit, 
-    p.totalcost AS totalplannedcommit, 
-    --pc.totalspend,
-    --pc.maxdate, 
-    --pc.mindate,
-    p.magencyacro,
-    p.magencyname,
-    --pc.typecategory,
-    p.ccpversion,
-    d.geom
-    FROM cpdb_projects as p,
-    --cpdb_projects_combined as pc, 
-    cpdb_dcpattributes as d
-    WHERE 
-    --p.maprojid = pc.maprojid AND
-    p.maprojid = d.maprojid
-    )
-SELECT S.*
-FROM summary s
-WHERE ST_GeometryType(geom)= 'ST_MultiPoint');
-
+WITH proj_pts AS (
+SELECT 
+p.maprojid,
+p.magency, 
+p.projectid, 
+p.description, 
+p.ccnonexempt,
+p.ccexempt, 
+p.citycost AS totalcityplannedcommit,
+p.nccstate, 
+p.nccfederal, 
+p.nccother, 
+p.noncitycost AS totalnoncityplannedcommit, 
+p.totalcost AS totalplannedcommit, 
+pc.totalspend,
+pc.maxdate, 
+pc.mindate,
+p.magencyacro,
+p.magencyname,
+pc.typecategory,
+p.ccpversion,
+d.geom
+FROM cpdb_projects as p,
+cpdb_projects_combined as pc, 
+cpdb_dcpattributes as d
+WHERE 
+p.maprojid = pc.maprojid AND
+p.maprojid = d.maprojid AND
+ST_GeometryType(geom)= 'ST_MultiPoint')
+SELECT p.*
+INTO cpdb_opendata_projects_pts
+FROM proj_pts as p;
 
 
 DROP TABLE IF EXISTS cpdb_opendata_projects_poly;
-CREATE TABLE cpdb_opendata_projects_poly AS (
-    WITH summary AS (
-    SELECT p.maprojid,
-    p.magency, 
-    p.projectid, 
-    p.description, 
-    p.ccnonexempt,
-    p.ccexempt, 
-    p.citycost AS totalcityplannedcommit,
-    p.nccstate, 
-    p.nccfederal, 
-    p.nccother, 
-    p.noncitycost AS totalnoncityplannedcommit, 
-    p.totalcost AS totalplannedcommit, 
-    pc.totalspend,
-    pc.maxdate, 
-    pc.mindate,
-    p.magencyacro,
-    p.magencyname,
-    pc.typecategory,
-    p.ccpversion,
-    d.geom
-    FROM cpdb_projects as p,
-    --cpdb_projects_combined as pc, 
-    cpdb_dcpattributes as d
-    WHERE 
-    --p.maprojid = pc.maprojid AND
-    p.maprojid = g.maprojid
-    )
-SELECT S.*
-FROM summary s
-WHERE ST_GeometryType(geom)= 'ST_MultiPolygon');
+WITH proj_poly AS (
+SELECT 
+p.maprojid,
+p.magency, 
+p.projectid, 
+p.description, 
+p.ccnonexempt,
+p.ccexempt, 
+p.citycost AS totalcityplannedcommit,
+p.nccstate, 
+p.nccfederal, 
+p.nccother, 
+p.noncitycost AS totalnoncityplannedcommit, 
+p.totalcost AS totalplannedcommit, 
+pc.totalspend,
+pc.maxdate, 
+pc.mindate,
+p.magencyacro,
+p.magencyname,
+pc.typecategory,
+p.ccpversion,
+d.geom
+FROM cpdb_projects as p,
+cpdb_projects_combined as pc, 
+cpdb_dcpattributes as d
+WHERE 
+p.maprojid = pc.maprojid AND
+p.maprojid = d.maprojid AND
+ST_GeometryType(geom)= 'ST_MultiPolygon')
+SELECT p.*
+INTO cpdb_opendata_projects_poly
+FROM proj_poly as p;
