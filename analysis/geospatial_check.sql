be2d3ab6ef14552d55d8b00d93146091196c1544
Jingyi Huang <jh5721@nyu.edu>
2022-08-01 12:19:45 -0400

    update

--- analysis/geospatial_check.sql
+++ analysis/geospatial_check.sql
@@ -16,12 +16,8 @@ from (
                               'projects_not_within_NYC' as field
 	from (SELECT a.projectid, a.magencyacr, a.typecatego
           FROM cpdb_dcpattributes_poly a, 
-               (SELECT ST_Union(geom) geom
-               FROM borough_boundaries_wi) combined 
+               (SELECT ST_Union(wkb_geometry) geom
+               FROM dcp_boroboundaries_wi) combined 
           WHERE NOT ST_WITHIN(a.geom, combined.geom)) tmp
-    union 
-    select jsonb_agg(tmp.admin_boundary_type)as values,'unique_admin_boundary_type' as field 
-    from (SELECT DISTINCT admin_boundary_type 
-          FROM cpdb_adminbounds) tmp 
     )t
 )
