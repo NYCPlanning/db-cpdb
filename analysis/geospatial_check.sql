9e7278a3d55704af79b3b720a81fbfcca67e7835
Jingyi Huang <jh5721@nyu.edu>
2022-08-01 13:50:32 -0400

    fix typo

--- analysis/geospatial_check.sql
+++ analysis/geospatial_check.sql
@@ -11,10 +11,10 @@ INSERT INTO geospatial_check_table (
 select :'ccp_v' as v, 	 
         jsonb_agg(t) as result
 from (
-    select jsonb_agg(json_build_object('projectid', tmp.projectid, 'magencyacr',tmp.magencyacr, 
+    select jsonb_agg(json_build_object('projectid', tmp.projectid, 'magencyacro',tmp.magencyacro, 
                               'typecatego', tmp.typecatego)) as values, 
                               'projects_not_within_NYC' as field
-	from (SELECT a.projectid, a.magencyacr, a.typecatego
+	from (SELECT a.projectid, a.magencyacro, a.typecatego
           FROM cpdb_dcpattributes_poly a, 
                (SELECT ST_Union(wkb_geometry) geom
                FROM dcp_boroboundaries_wi) combined 
