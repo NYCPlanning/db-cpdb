362e1365c9198932487f62ef572300cd962aefb3
Finn Van Krieken <finnvankrieken@DCP-APPLE-2153.local>
2023-03-15 14:47:16 -0400

    Actual fix of frame.append use

--- python/dot_bridges.py
+++ python/dot_bridges.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 import pandas as pd
 import os
 from utils import psql_insert_copy
@@ -14,36 +14,30 @@ def fms_parse(x):
     if "," in x.fms_id:
         fms_ids = x.fms_id.split(",")
         fms_ids = [i.strip() for i in fms_ids]
-        row = x.to_frame().T
-        rows = x.to_frame().T
-        while len(rows) < len(fms_ids):
-            rows = rows.append(row)
+        rows = pd.DataFrame([ x for _ in fms_ids])
         rows["fms_id"] = fms_ids
     # case 2: / delimited fms ids
     elif "/" in x.fms_id:
         fms_ids = x.fms_id.split("/")
         fms_ids[1:] = [fms_ids[0][0:-1] + i for i in fms_ids[1:]]
-        row = x.to_frame().T
-        rows = x.to_frame().T
-        while len(rows) < len(fms_ids):
-            rows = rows.append(row)
+        rows = pd.DataFrame([ x for _ in fms_ids])
         rows["fms_id"] = fms_ids
     else:
-        rows = x.T
+        rows = pd.DataFrame(x)
     return rows
 
+with engine.begin() as conn:
+    # makes selection
+    bridges = pd.read_sql_query(
+        text("SELECT * FROM dot_projects_bridges WHERE fms_id is not NULL"), conn
+    )
 
-# makes selection
-bridges = pd.read_sql_query(
-    "SELECT * FROM dot_projects_bridges WHERE fms_id is not NULL", engine
-)
+    # fms_id cleaning
+    bridges_cleaned = pd.concat([ fms_parse(bridges.iloc[i, :]) for i in range(len(bridges))])
 
-# fms_id cleaning
-bridges_cleaned = pd.concat([fms_parse(bridges.iloc[i, :]) for i in range(len(bridges))])
+    bridges_cleaned["ogc_fid"] = bridges_cleaned["ogc_fid"].astype("str")
 
-bridges_cleaned["ogc_fid"] = bridges_cleaned["ogc_fid"].astype("str")
-
-# write new table to postgres
-bridges_cleaned.to_sql(
-    "dot_projects_bridges_byfms", engine, if_exists="replace", index=False, method=psql_insert_copy
-)
+    # write new table to postgres
+    bridges_cleaned.to_sql(
+        "dot_projects_bridges_byfms", conn, if_exists="replace", index=False, method=psql_insert_copy
+    )
